577ab168f6de7bc234265b4cd08f5a1001747084
Luciano0981 <lucianoduran0981@gmail.com>
2023-04-02 14:54:22 -0600

    add code
    En main Principal hacemos en los ciclos while y for para sus solicitud con sus datos respectivos aun no esta en funcionada en codigo

--- ProyectoLuisE/src/proyectoluise/FideTowerDefense.java
+++ ProyectoLuisE/src/proyectoluise/FideTowerDefense.java
@@ -8,13 +8,47 @@ package proyectoluise;
  *
  * @author Luis
  */
-public class FideTowerDefense {
+class FideTowerDefense {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        Castillo castilloJugador = new Castillo();
+        Castillo castilloCPU = new Castillo();
+
+        int oleada = 1;
+
+        while (!castilloJugador.destruido && !castilloCPU.destruido) {
+            System.out.println("Oleada: " + oleada);
+            int tropasMaximas = oleada + 4;
+
+            Tropa[] tropasJugador = new Tropa[tropasMaximas];
+            Tropa[] tropasCPU = new Tropa[tropasMaximas - 1];
+
+            System.out.println("Elige tus tropas (Mago, Caballero, Arquero):");
+            for (int i = 0; i < tropasMaximas; i++) {
+                System.out.print("Tropa " + (i + 1) + ": ");
+                String tipo = reader.readLine();
+                tropasJugador[i] = new Tropa(tipo);
+            }
+
+            generarTropasCPU(tropasCPU);
+
+            System.out.println("Las primeras 3 tropas de la CPU son:");
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Tropa " + (i + 1) + ": " + tropasCPU[i].tipo);
+            }
+
+            for (int i = 0; i < tropasMaximas; i++) {
+                for (int j = 0; j < tropasMaximas - 1; j++) {
+                    if (tropasJugador[i] != null && tropasCPU[j] != null) {
+                        if (tropasJugador[i].ganarEnCombate(tropasCPU[j])) {
+                            tropasCPU[j] = null;
+                        } else {
+                            tropasJugador[i] = null;
+}
+}
+}
+}
+        }
     }
-    
 }
