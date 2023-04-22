bf3fd1baf4c51e4bf1d2cdb540fc80ef38947d33
Luciano0981 <lucianoduran0981@gmail.com>
2023-04-21 19:04:07 -0600

    add code
    Modificacion y union de la clase cola

--- ProyectoLuisE/src/proyectoluise/FideTowerDefense.java
+++ ProyectoLuisE/src/proyectoluise/FideTowerDefense.java
@@ -9,8 +9,12 @@ import java.io.InputStreamReader;
  *
  * @author Luis
  */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class FideTowerDefense {
-    static BufferedReader reader = 
+    static BufferedReader reader =
             new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
@@ -24,91 +28,118 @@ class FideTowerDefense {
             System.out.println("Oleada: " + oleada);
             int tropasMaximas = oleada + 4;
 
-            Tropa[] tropasJugador = new Tropa[tropasMaximas];
-            Tropa[] tropasCPU = new Tropa[tropasMaximas - 1];
+            Cola tropasJugador = new Cola();
+            Cola tropasCPU = new Cola();
 
             System.out.println("Elige tus tropas (Mago, Caballero, Arquero):");
             for (int i = 0; i < tropasMaximas; i++) {
                 System.out.print("Tropa " + (i + 1) + ": ");
                 String tipo = reader.readLine();
-                tropasJugador[i] = new Tropa(tipo);
+                tropasJugador.agregar(new Tropa(tipo));
             }
 
-            generarTropasCPU(tropasCPU);
+            generarTropasCPU(tropasCPU, tropasMaximas - 1);
 
             System.out.println("Las primeras 3 tropas de la CPU son:");
+            Cola copiaTropasCPU = copiarCola(tropasCPU);
             for (int i = 0; i < 3; i++) {
-                System.out.println("Tropa " + (i + 1) + ": " + tropasCPU[i].tipo);
+                Tropa t = copiaTropasCPU.sacar();
+                System.out.println("Tropa " + (i + 1) + ": " + t.tipo);
             }
 
             for (int i = 0; i < tropasMaximas; i++) {
+                Tropa jugadorTropa = tropasJugador.sacar();
+                if (jugadorTropa == null) break;
                 for (int j = 0; j < tropasMaximas - 1; j++) {
-                    if (tropasJugador[i] != null && tropasCPU[j] != null) {
-                        if (tropasJugador[i].ganarEnCombate(tropasCPU[j])) {
-                            tropasCPU[j] = null;
-                        } else {
-                            tropasJugador[i] = null;
-}
-}
-}
-}
- for (int i = 0; i < tropasMaximas; i++) {
-            if (tropasJugador[i] != null) {
-                castilloCPU.recibirDanio(tropasJugador[i].puntosDeDanio);
+                    Tropa cpuTropa = tropasCPU.sacar();
+                    if (cpuTropa == null) break;
+
+                    if (jugadorTropa.ganarEnCombate(cpuTropa)) {
+                        // No se añade la tropa de la CPU a la cola
+                    } else {
+                        tropasCPU.agregar(cpuTropa);
+                    }
+                }
             }
-            if (i < tropasMaximas - 1 && tropasCPU[i] != null) {
-                castilloJugador.recibirDanio(tropasCPU[i].puntosDeDanio);
+
+            Tropa t;
+            while ((t = tropasJugador.sacar()) != null) {
+                castilloCPU.recibirDanio(t.puntosDeDanio);
             }
+            while ((t = tropasCPU.sacar()) != null) {
+                castilloJugador.recibirDanio(t.puntosDeDanio);
+            }
+
+            System.out.println("Puntos de vida del castillo del jugador: "
+                    + castilloJugador.puntosDeVida);
+            System.out.println("Puntos de vida del castillo de la CPU: "
+                    + castilloCPU.puntosDeVida);
+
+            oleada++;
         }
 
-        System.out.println("Puntos de vida del castillo del jugador: "
-                + castilloJugador.puntosDeVida);
-        System.out.println("Puntos de vida del castillo de la CPU: " 
-                + castilloCPU.puntosDeVida);
-
-        oleada++;
-    }
-
-    if (castilloJugador.destruido) {
-        System.out.println
-        ("¡El castillo del jugador ha sido destruido! La CPU gana.");
-    } else {
-        System.out.println
-        ("¡El castillo de la CPU ha sido destruido! El jugador gana.");
-    }
-}
-
-static void generarTropasCPU(Tropa[] tropasCPU) {
-    String[] tipos = {"Mago", "Caballero", "Arquero"};
-    int limiteTropas = (int) Math.ceil((tropasCPU.length * 75.0) / 100.0);
-
-    for (int i = 0; i < tropasCPU.length; i++) {
-        int randomIndex = (int) (Math.random() * tipos.length);
-        tropasCPU[i] = new Tropa(tipos[randomIndex]);
-    }
-
-    int[] contadorTipos = {0, 0, 0};
-    for (int i = 0; i < tropasCPU.length; i++) {
-        for (int j = 0; j < tipos.length; j++) {
-            if (tropasCPU[i].tipo.equals(tipos[j])) {
-                contadorTipos[j]++;
-            }
+        if (castilloJugador.destruido) {
+            System.out.println
+                    ("¡El castillo del jugador ha sido destruido! La CPU gana.");
+        } else {
+            System.out.println
+                    ("¡El castillo de la CPU ha sido destruido! El jugador gana.");
         }
     }
+    static void generarTropasCPU(Cola tropasCPU, int tropasMaximas) {
+        String[] tipos = {"Mago", "Caballero", "Arquero"};
+        int limiteTropas = (int) Math.ceil((tropasMaximas * 75.0) / 100.0);
 
-    for (int i = 0; i < contadorTipos.length; i++) {
-        if (contadorTipos[i] > limiteTropas) {
-            int count = contadorTipos[i] - limiteTropas;
-            for (int j = 0; j < tropasCPU.length && count > 0; j++) {
-                if (tropasCPU[j].tipo.equals(tipos[i])) {
-                    int randomIndex = (int) (Math.random() * tipos.length);
-                    tropasCPU[j] = new Tropa(tipos[randomIndex]);
-                    count--;
+        for (int i = 0; i < tropasMaximas; i++) {
+            int randomIndex = (int) (Math.random() * tipos.length);
+            tropasCPU.agregar(new Tropa(tipos[randomIndex]));
+        }
+
+        int[] contadorTipos = {0, 0, 0};
+        Cola copiaTropasCPU = copiarCola(tropasCPU);
+        for (int i = 0; i < tropasMaximas; i++) {
+            Tropa t = copiaTropasCPU.sacar();
+            if (t == null) break;
+            for (int j = 0; j < tipos.length; j++) {
+                if (t.tipo.equals(tipos[j])) {
+                    contadorTipos[j]++;
                 }
             }
         }
+
+        for (int i = 0; i < contadorTipos.length; i++) {
+    if (contadorTipos[i] > limiteTropas) {
+        int count = contadorTipos[i] - limiteTropas;
+        Cola nuevaCola = new Cola();
+        Tropa t;
+        while ((t = tropasCPU.sacar()) != null) {
+            if (t.tipo.equals(tipos[i]) && count > 0) {
+                int randomIndex = (int) (Math.random() * tipos.length);
+                nuevaCola.agregar(new Tropa(tipos[randomIndex]));
+                count--;
+            } else {
+                nuevaCola.agregar(t);
+            }
+        }
+        tropasCPU = nuevaCola;
     }
 }
+
+    }
+
+    static Cola copiarCola(Cola colaOriginal) {
+        Cola copia = new Cola();
+        Cola colaTemp = new Cola();
+        Tropa t;
+        while ((t = colaOriginal.sacar()) != null) {
+            copia.agregar(t);
+            colaTemp.agregar(t);
+        }
+        while ((t = colaTemp.sacar()) != null) {
+            colaOriginal.agregar(t);
+        }
+        return copia;
+    }
 }
-    
+
 
